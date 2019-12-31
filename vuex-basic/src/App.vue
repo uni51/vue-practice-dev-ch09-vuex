57b9c8e9348bdbc49540a415a2b3c5fe22fac351
uni51 <uni51@users.noreply.github.com>
2020-01-01 06:45:59 +0900

    9-2 Vuexの基本

---
+++ vuex-basic/src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <input type="button" value="-" v-on:click="minus" />    
+    {{count}}
+    <input type="button" value="+" v-on:click="plus" />
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  name: 'app',
+
+  /*
+  computed: {
+    // 現在のカウント値を取得
+    count() {
+      return this.$store.state.count
+    }
+  },
+  */
+
+  computed: mapState(['count']),
+
+  /*
+  // 配列／オブジェクト形式のマッピングを統合
+  computed: {
+    ...mapState([ 'count' ]),
+    ...mapState({ countNumber: 'count' })
+  },
+  */
+
+  methods: {
+    minus() {
+      this.$store.commit('minus')
+      // console.log(this.$store.state.count)
+    },
+    plus() {
+      this.$store.commit('plus')
+      // console.log(this.$store.state.count)
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
